f6067da8a3ba996b4de4dc16885361bce5798efa
PriyankaRumal <priyankarumal182000@gmail.com>
2022-10-21 11:37:10 +0530

    UseCase2

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UC2DailyWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagePerHr = 20;
+            int Wage = 0,empHr=0;
+            Random random = new Random();
+            int value = random.Next(0,2);//only generate value bt 0-2 means 0,1
+            //0 is for present 
+            //1 is for absent
+            if(value==0)
+            {
+                empHr = 8;
+            }
+            else
+            {
+                empHr = 0;
+            }
+            Wage = wagePerHr * empHr;
+            Console.WriteLine("Total Wage Of The Day :" + Wage);
+        }
+    }
+}
